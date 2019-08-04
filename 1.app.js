31f9089651455edd30a1f9cbd27dbba7162ee3fa
邹宇鸿 <790381001@qq.com>
2019-08-04 19:26:56 +0800

    第二次修改

---
+++ 1.app.js
@@ -0,0 +1,25 @@
+
+// 引入模块
+const express = require('express');
+// 创建服务器
+const app = express();
+const router = require('./2.router');
+
+// 绑定端口和ip
+app.listen(3000, () => {
+  console.log('http://127.0.0.1:8080');
+})
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+});
+// 处理静态资源
+app.use('/assets',express.static('assets'));
+app.use('/uploads',express.static('uploads'));
+
+// 设置默认模板
+app.set('view engine','ejs');
+app.set('/views',__dirname+'views');
+
+
+app.use(router);
---
+++ 2.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var router = express.Router();
+
+router.get('/admin',(req,res)=>{
+    res.render('admin/index.ejs');
+})
+
+router.get('/admin',(req,res)=>{
+    res.render('admin/index.ejs');
+})
